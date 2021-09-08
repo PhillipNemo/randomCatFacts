31166cb27f09578c9bf18ce1c9285e53db1499a0
PhillipNemo <philipsonokere@gmail.com>
2021-09-08 02:51:12 +0100

    catfats

---
+++ script.js
@@ -0,0 +1,29 @@
+const factBox = document.querySelector('#fact')
+const getFactBtn = document.querySelector('#btn')
+let catFacts = [];
+
+
+
+fetch('https://codexpression.github.io/catz/catz.json').then(response => {
+    console.log(response);
+    return response.json();
+
+})
+.then(data => {
+   let allFacts = data.all
+   localStorage.setItem('catFacts',JSON.stringify(allFacts));
+//    catFacts = [...allFacts]
+})
+.catch(err => {
+    console.log('You may be having network issues or it\'s possible the URL you supplied is invalid. Plese do check and reload the page. \n', err);
+})
+catFacts = [...JSON.parse(localStorage.getItem('catFacts'))]
+console.log(catFacts)
+
+function generateFact(){
+    let randomNumber = Math.floor(Math.random() *catFacts.length);
+    let fact = catFacts[randomNumber];
+    factBox.innerHTML = fact.text;
+}
+
+getFactBtn.onclick = generateFact;
